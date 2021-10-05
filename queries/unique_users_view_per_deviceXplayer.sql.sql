35fb8c621945fa55e58892059524005a7a57f8d9
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2021-10-04 23:34:49 -0300

    :recycle: refactor players per devices query

--- queries/unique_users_view_per_deviceXplayer.sql.sql
+++ queries/unique_users_view_per_deviceXplayer.sql.sql
@@ -1,10 +1,28 @@
-SELECT Players.name as name, count(distinct user_id) AS unique_viewers, Devices.name as device
-FROM Views
-INNER JOIN Users ON Views.user_id = Users.id
-INNER JOIN GenreTitles ON Views.title_id = GenreTitles.title_id
-INNER JOIN Genres ON genre_id = Genres.id
-INNER JOIN Players ON player_id = Players.id
-INNER JOIN Devices ON device_id = Devices.id
-WHERE devices.name='desktop'
-GROUP BY Players.name, Devices.name
-ORDER BY unique_viewers DESC
+CREATE OR REPLACE FUNCTION count_players_per_device (int)
+    RETURNS TABLE (
+        total_views bigint,
+		name varchar
+    )
+    AS $$
+    SELECT
+        count(*) AS total_views,
+		Players.name
+    FROM
+        Views
+        JOIN Players ON player_id = Players.id
+        JOIN Devices ON device_id = Devices.id
+	WHERE
+		device_id = $1 
+    GROUP BY
+        Players.name
+    ORDER BY
+        total_views DESC
+$$
+LANGUAGE SQL;
+
+
+SELECT
+    counter.total_views AS total_views,
+	counter.name AS player,
+    d.name as on_device
+FROM Devices d, count_players_per_device(d.id) AS counter;
